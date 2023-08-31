edd2c02157b5549c458ac78bf1bcd11d5a5fcd88
DawidKowalczyk93 <bandiwaves@gmail.com>
2023-08-31 14:16:58 +0200

    init

---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,50 @@
+import {useState} from "react";
+import {PlusIcon} from '@heroicons/react/24/solid';
+
+const CustomForm = ({addTask}) => {
+
+    const [task, setTask] = useState('');
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTask({
+            name: task,
+            check: false,
+            id: Date.now()
+        });
+        setTask("");
+    };
+
+    return (
+        <form
+            onSubmit={handleFormSubmit}
+        >
+            <input
+                type='text'
+                id='task'
+                className='input'
+                value={task}
+                onInput={e => setTask(e.target.value)}
+                required
+                autoFocus
+                maxLength={60}
+                placeholder='Enter Task'
+            />
+            <label
+                htmlFor='task'
+                className='label'
+            ></label>
+            <button
+                className='button'
+                aria-label='Add Task'
+                type='submit'
+            >
+                <PlusIcon />
+            </button>
+
+
+
+        </form>
+    )
+};
+
+export default CustomForm
